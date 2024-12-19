34bd548bca7dcf5c12ec1f3dd6d2540a78574520
syed-zunair-gillani <dev.zunaair@gmail.com>
2024-12-19 19:33:42 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import CertificationsAndMemberships from "@/components/certifications-and-memberships/CertificationsAndMemberships";
 import Main from "@/components/hero/main";
-import Image from "next/image";
 
 export default function Home() {
   return (
