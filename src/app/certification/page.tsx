191a34534dae60741e50ce5c876b7be8bf5dfb0d
syed-zunair-gillani <dev.zunaair@gmail.com>
2024-12-20 18:00:52 +0500

    up

---
+++ src/app/certification/page.tsx
@@ -0,0 +1,19 @@
+
+import Logocard from "@/components/certification/Logocard";
+import Image from "next/image";
+import Title2 from "@/components/certification/Title2";
+import Title from "@/components/certification/Title";
+import Logocard2 from "@/components/certification/logocard2";
+
+
+export default function Home() {
+  return (
+    <main className="font-calibri">
+    <Title />
+    <Logocard/>
+    <Title2 />
+    <Logocard2/>
+    
+    </main>
+  );
+}
