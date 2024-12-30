a8f9364eaf92723a319d4e04b6337e14af6a29af
syed-zunair-gillani <dev.zunaair@gmail.com>
2024-12-31 02:11:20 +0500

    update

---
+++ src/components/renderRichText.jsx
@@ -0,0 +1,36 @@
+export const renderRichText = (blocks) => {
+    return blocks.map((block, index) => {
+      const { type, level, children } = block;
+  
+      // Render based on block type
+      switch (type) {
+        case "heading":
+          const HeadingTag = `h${level}`; // Dynamically determine heading level
+          if(HeadingTag === "h2"){
+            return (
+              <HeadingTag key={index} className="font_franklin mb-5">
+                {children.map((child, childIndex) => child.text)}
+              </HeadingTag>
+            );
+          }else{
+            return (
+              <h3 key={index} className="!font-xl mb-5 !text-[#3E3E3E]">
+                {children.map((child, childIndex) => child.text)}
+              </h3>
+            );
+          }
+          
+  
+        case "paragraph":
+          return (
+            <p key={index} className="font-light !text-[#3E3E3E]">
+              {children.map((child, childIndex) => child.text)}
+            </p>
+          );
+  
+        default:
+          return null; // Handle other block types as needed
+      }
+    });
+  };
+  
