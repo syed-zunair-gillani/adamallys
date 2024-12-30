0572778f541905b08cd843a6b5ab4e67f6ebdef3
Muhammad Shoaib <m.xhoaib1@gmail.com>
2024-12-31 04:17:24 +0500

    contact page integration

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import { getContactUs } from '@/services'
+import ContactTemplate from '@/templates/contact'
+
+const Contact = async () => {
+    const data = await getContactUs();
+    return (
+        <ContactTemplate {...data} />
+    )
+}
+
+export default Contact
