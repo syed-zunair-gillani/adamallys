3814a0c22af194943cd5acb1d9e5ceef16d75bc9
Muhammad Shoaib <m.xhoaib1@gmail.com>
2024-12-31 04:11:27 +0500

    pages integration

--- src/app/news-&-events/page.jsx
+++ src/app/news-&-events/page.jsx
@@ -1,16 +1,36 @@
-
+import { getNewsAndEvents } from "@/services";
 import Main from "@/components/news-events/main";
 import Card from "@/components/news-events/card";
-import { getNewsAndEvents } from "@/services";
 
 export default async function News() {
   const data = await getNewsAndEvents()
-  console.log('data', data);
+  const newsData = data?.data?.map(({ attributes }) => ({
+    slug: attributes?.Slug,
+    title: attributes?.title,
+    date: attributes?.createdAt,
+    excerpt: attributes?.excerpt,
+    image: attributes?.Image?.data?.attributes?.url,
+  }));
 
   return (
     <>
-      <Main />
-      <Card />
+      <Main news={newsData.slice(0, 3)} />
+      <div className="container mx-auto flex flex-wrap gap-[23px] px-4 mt-[23px]">
+        {
+          [...newsData, ...newsData, ...newsData]?.map((news, index) =>
+            <div key={news?.title + index} className="basis-full sm:basis-[40%] flex-1 lg:basis-[30%]">
+              <Card {...news} />
+            </div>
+          )
+        }
+      </div>
+      <div className="flex justify-center items-center my-[64px]">
+        <div className="w-[157px] text-[#2E368F] flex justify-center items-center border px-4 py-2 md:px-5 md:py-3 border-[#2E368F]">
+          <button className="text-[14px] md:text-[16px] ">
+            Load More
+          </button>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/news-events/card.tsx
+++ src/components/news-events/card.tsx
@@ -1,38 +1,21 @@
-import React from 'react';
+import dayjs from 'dayjs';
+import Link from 'next/link';
 import { IoIosArrowForward } from "react-icons/io";
 
-function Card() {
-  const cards = [
-    { 
-      logo: '/images/card.svg', 
-      title: 'ISO 9001:2015',
-      date: '25 January 2024',
-      description: 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used.',
-      link: '#'
-    },
- 
-  ];
-
+function Card({ title, image, date, slug }: any) {
   return (
-    <div className="container mx-auto px-4 py-32">
-      <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-        {cards.map((card, index) => (
-          <div
-            key={index}
-            className="flex flex-col items-center mx-auto w-full max-w-[600px] h-auto sm:h-[300px] justify-center mt-10 duration-300"
-          >
-            <img src={card.logo} alt={card.title} className="w-full" />
-            <div className="bg-[#F5F6F8] w-full py-8 px-10">
-              <p className="text-[#2E368F]">{card.date}</p>
-              <h3 className="text-[25px] font-[300] text-[#8B8B8B] py-3">{card.description}</h3>
-              <span className="flex items-center gap-4 text-[#2E368F] mt-4">
-                <a href={card.link}>Read More</a>
-                <IoIosArrowForward />
-              </span>
-            </div>
-          </div>
-        ))}
-      </section>
+    <div
+      className="flex flex-col items-center mx-auto w-full max-w-[600px] justify-center duration-300"
+    >
+      <img src={image} alt={title} className="w-full" />
+      <div className="bg-[#F5F6F8] w-full py-8 px-10">
+        <p className="text-[#2E368F]">{dayjs(date).format('DD MMMM YYYY')}</p>
+        <h3 className="text-[25px] font-[300] text-[#8B8B8B] py-3">{title}</h3>
+        <span className="flex items-center gap-4 text-[#2E368F] mt-4">
+          <Link href={`/${slug}`}>Read More</Link>
+          <IoIosArrowForward />
+        </span>
+      </div>
     </div>
   );
 }
