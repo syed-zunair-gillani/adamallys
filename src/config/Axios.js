f8431479a3b7f9b619e9351811a02e3a1e9a9113
syed-zunair-gillani <dev.zunaair@gmail.com>
2025-01-02 21:40:25 +0500

    up

--- src/config/Axios.js
+++ src/config/Axios.js
@@ -3,8 +3,8 @@ import axios from "axios"
 export const Axios = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_BACKEND_PUBLIC_BASE_URL}/api/`,
   timeout: 5000,
-  headers: {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_PREVIEW_TOKEN}`,
-  },
+  // headers: {
+  //   "Content-Type": "application/json",
+  //   Authorization: `Bearer ${process.env.NEXT_PUBLIC_PREVIEW_TOKEN}`,
+  // },
 });
