e2302d23c801d340ac76003636fa85a64f5c3c54
Muhammad Shoaib <m.xhoaib1@gmail.com>
2025-01-07 13:48:08 +0500

    mega menu fixes

--- src/components/header/MegaMenu.jsx
+++ src/components/header/MegaMenu.jsx
@@ -1,4 +1,5 @@
 'use client'
+import { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { usePathname } from 'next/navigation';
@@ -8,6 +9,7 @@ import styles from './style.module.css';
 
 const MegaMenu = ({ title, links }) => {
   const pathname = usePathname();
+  const [isShipSupplyHovered, setIsShipSupplyHovered] = useState(false);
   const isShipSupplyPage = pathname === "/products-&-services"
   const isIndustrialPage = pathname === "/industrial-&-energy-sector-supplies"
   const isMarineLogisticsPage = pathname === "/marine-logistics-&-warehousing"
@@ -21,7 +23,7 @@ const MegaMenu = ({ title, links }) => {
             style={{ zIndex: 999, boxShadow: '0px 2px 12.5px 0px rgba(46, 54, 143, 0.09)' }}
             className={`absolute flex top-[36px] bg-white text-theme-main z-[999]`}
           >
-            <ul className="min-w-[310px] flex flex-col gap-7 py-[31px] px-[35px]">
+            <ul className="relative min-w-[310px] flex flex-col gap-7 py-[31px] px-[35px]">
               {links?.length ?
                 links?.map((linkItem, index) =>
                   <li key={index} className="relative group">
@@ -35,15 +37,26 @@ const MegaMenu = ({ title, links }) => {
                   </li>
                 ) :
                 <>
-                  <li className="relative group">
+                  <li
+                    onMouseEnter={() => setIsShipSupplyHovered(true)}
+                    onMouseLeave={() => setIsShipSupplyHovered(false)}
+                    className='top-[31px] w-full h-[25px] absolute' />
+                  <li
+                    onMouseEnter={() => setIsShipSupplyHovered(true)}
+                    onMouseLeave={() => setIsShipSupplyHovered(false)}
+                    className="relative"
+                  >
                     <div className="flex items-center justify-between">
                       <Link
                         href="/products-&-services"
-                        className={`whitespace-nowrap ${isShipSupplyPage ? 'font-bold' : ''} hover:font-bold`}
+                        className={`whitespace-nowrap ${(isShipSupplyPage || isShipSupplyHovered) ? 'font-bold' : ''} hover:font-bold`}
                       >
                         Ship Supply
                       </Link>
-                      <Image src={'/svg/arrow_forward_link.svg'} alt='arrow' width={13} height={13} />
+                      {
+                        isShipSupplyHovered &&
+                        <Image src={'/svg/arrow_forward_link.svg'} alt='arrow' width={13} height={13} />
+                      }
                     </div>
                     {isShipSupplyPage && <div className='mt-2 w-[18px] h-[2px] bg-theme-main' />}
                   </li>
@@ -67,10 +80,14 @@ const MegaMenu = ({ title, links }) => {
               }
             </ul>
             {!title &&
-              <div className='flex product-services-menu'>
+              <div
+                onMouseEnter={() => setIsShipSupplyHovered(true)}
+                onMouseLeave={() => setIsShipSupplyHovered(false)}
+                className={`${isShipSupplyHovered ? 'flex' : 'hidden'}`}
+              >
                 <div className='w-[2px] bg-[rgba(0,0,0,0.075)]' />
                 <div
-                  className="w-full min-w-[505px] flex  w-full bg-white text-theme-main group-hover:flex">
+                  className={`w-full min-w-[505px] flex  w-full bg-white text-theme-main`}>
                   <ul className="grid grid-cols-2 gap-7 py-[31px] px-[35px]">
                     {shipSupplyPaginationData?.map((page) => (
                       <li
