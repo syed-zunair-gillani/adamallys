b943e907376b5360a6cdf86aea9d0b48d2b0a8ec
Muhammad Shoaib <m.xhoaib1@gmail.com>
2025-01-09 02:38:55 +0500

    contact page responsiveness

--- src/components/MapComponent/index.jsx
+++ src/components/MapComponent/index.jsx
@@ -2,8 +2,12 @@ import Image from "next/image";
 
 const MapComponent = () => {
     return (
-        <section>
-            <Image src="/svg/contact-us-map.svg" alt="" className="w-full" width={1200} height={600}/>
+        <section className="min-h-[482px]">
+            <Image
+                style={{
+                    objectFit: 'cover'
+                }}
+                src="/svg/contact-us-map.svg" alt="" className="w-full min-h-[482px]" width={1200} height={600} />
         </section>
     );
 };
