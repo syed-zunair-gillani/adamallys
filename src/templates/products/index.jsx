531d56997c047e8ada5c45e3b3e679ac528c094f
Muhammad Shoaib <m.xhoaib1@gmail.com>
2025-01-09 21:28:17 +0500

    products fixes

--- src/templates/products/index.jsx
+++ src/templates/products/index.jsx
@@ -67,7 +67,7 @@ const ProductsTemplate = ({ data, categories, specificCategorries, baseCategorri
               className="w-full lg:max-w-[414px] h-[55px] px-2 py-2 border border-theme-main focus:outline-none text-theme-main focus:text-theme-main appearance-none"
             >
               <option value="" disabled selected>
-                Provisions
+                Select Category
               </option>
               {
                 baseCategorries?.map((item, idx) => (
@@ -86,7 +86,7 @@ const ProductsTemplate = ({ data, categories, specificCategorries, baseCategorri
           </p>
 
           <div className="flex flex-col md:flex-row md:items-center gap-2 md:gap-[24px]">
-            <p className='text-theme-main text-base md:text-lg md:leading-[26px] font_calibri'>Fresh Vegetables 1421 Search result</p>
+            <p className='text-theme-main text-base md:text-lg md:leading-[26px] font_calibri'>Fresh Vegetables {products?.data?.length} Search result</p>
             <div className="flex gap-2">
               <div className="md:hidden relative flex items-center flex-1 lg:flex-auto min-w-[220px]">
                 <select
