9c2b3ce4a1dfd34b4c29eccf0d2997b98ac321c7
Muhammad Shoaib <m.xhoaib1@gmail.com>
2025-01-10 00:29:51 +0500

    Corporate Information heading font fixes

--- src/templates/about/CorporateInformation.jsx
+++ src/templates/about/CorporateInformation.jsx
@@ -18,7 +18,7 @@ const CorporateInformation = ({ data }) => {
             <p className='opacity-0'>i</p>
           </div>
           <div className='basis-[50%] md:mt-20 flex flex-col justify-center z-[10]'>
-            <p className='font_calibri capitalize text-[25px] md:text-left font-bold text-center md:text-[59px] leading-[49px] mb-[7px] md:pb-[26px]'>{data?.ci_title}</p>
+            <p className='font_franklin capitalize text-[25px] md:text-left text-center md:text-[59px] leading-[49px] mb-[7px] md:pb-[26px] text-[#020911]'>{data?.ci_title}</p>
             <p className='font_calibri capitalize md:text-base text-center text-xs md:text-left md:leading-[26px] font-light'>{data?.ci_info}</p>
           </div>
         </div>
