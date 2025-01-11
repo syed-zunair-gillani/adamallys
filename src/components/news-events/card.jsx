853c2e968988d41edce87807750174ce7efe54c9
syed-zunair-gillani <dev.zunaair@gmail.com>
2025-01-11 15:19:46 +0500

    news and events single

--- src/components/news-events/card.jsx
+++ src/components/news-events/card.jsx
@@ -10,9 +10,11 @@ function Card({ title, image, date, slug }) {
       <img src={image} alt={title} className="w-full" />
       <div className="bg-[#F5F6F8] w-full py-8 px-10">
         <p className="font_calibri text-[14px] text-[#2E368F] text-center md:text-left">{dayjs(date).format('DD MMMM YYYY')}</p>
-        <h3 className="font_calibri text-[14px] md:text-[25px] font-[300] text-[#8B8B8B] py-3 text-center md:text-left">{title}</h3>
+        <h3 className="font_calibri cursor-pointer text-[14px] md:text-[25px] font-[300] text-[#8B8B8B] py-3 text-center md:text-left">
+          <Link href={`/news-&-events/${slug}`}>{title}</Link>
+          </h3>
         <span className="font_calibri flex items-center justify-center md:justify-start gap-4 text-[#2E368F] md:mt-4 text-center md:text-left">
-          <Link className='font_calibri' href={`/${slug}`}>Read More</Link>
+          <Link className='font_calibri' href={`/news-&-events/${slug}`}>Read More</Link>
           <IoIosArrowForward />
         </span>
       </div>
