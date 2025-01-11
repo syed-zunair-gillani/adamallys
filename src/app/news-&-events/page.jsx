e03928872031a01bd7b09ad967fb3b68565ba4ca
Muhammad Shoaib <m.xhoaib1@gmail.com>
2025-01-11 21:09:36 +0500

    load more button fixes

--- src/app/news-&-events/page.jsx
+++ src/app/news-&-events/page.jsx
@@ -18,20 +18,22 @@ export default async function News() {
   return (
     <>
       <Main news={sortedNewsData.slice(0, 3)} />
-      <div className="container mx-auto flex flex-wrap gap-[23px] mt-[23px] px-[18px]">
+      <div className="container mx-auto flex flex-wrap gap-[23px] mt-[23px] px-[18px] mb-6">
         {sortedNewsData?.slice(3)?.map((news, index) =>
           <div key={news?.title + index} className="basis-full sm:basis-[40%] flex-1 lg:basis-[30%]">
             <Card {...news} />
           </div>
         )}
       </div>
-      <div className="flex justify-center items-center my-6 lg:my-[64px]">
-        <div className="w-[157px] text-[#2E368F] flex justify-center items-center border py-2 md:px-5 md:py-3 border-[#2E368F]">
-          <button className="text-[14px] md:text-[16px] ">
-            Load More
-          </button>
+      {(newsData?.length > 3) &&
+        <div className="flex justify-center items-center mt-6 lg:my-[64px]">
+          <div className="w-[157px] text-[#2E368F] flex justify-center items-center border py-2 md:px-5 md:py-3 border-[#2E368F]">
+            <button className="text-[14px] md:text-[16px] ">
+              Load More
+            </button>
+          </div>
         </div>
-      </div>
+      }
     </>
   );
 }
